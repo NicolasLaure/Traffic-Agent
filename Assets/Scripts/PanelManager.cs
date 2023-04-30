bcb4f632c65624f819ba462aa51b6acccab4c72b
Dario <otrantodario@gmail.com>
2023-04-30 00:17:25 -0300

    Fix Game Window Instances
    We can no longer instantiate multiple game windows

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -6,8 +6,12 @@ public class PanelManager : MonoBehaviour
 {
     public void WindowInstantiate(GameObject windowPrefab)
     {
-
-        GameObject newWindow =  Instantiate(windowPrefab, GameObject.Find("Canvas").transform);
+        var gameWindow = GameObject.FindGameObjectWithTag("Game Window");
+        if (gameWindow == null) 
+        {
+            GameObject newWindow = Instantiate(windowPrefab, GameObject.Find("Canvas").transform);
+        }
+        
         //Instantiate()
         //newWindow.transform.SetParent(GameObject.Find("Canvas").transform);
     }
