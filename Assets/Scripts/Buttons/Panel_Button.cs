899773e74b5b995771ee793cac9c8087889e7f3d
Dario <otrantodario@gmail.com>
2023-04-29 21:44:22 -0300

    Add hide and show previous panel function

--- Assets/Scripts/Buttons/Panel_Button.cs
+++ Assets/Scripts/Buttons/Panel_Button.cs
@@ -5,6 +5,7 @@ public class Panel_Button : MonoBehaviour
     // The ID of the panel we want to show
     public string panelID;
     private Panel_Manager _panel_Manager;
+    public Panel_Behaviour _Behaviour;
 
     private void Start()
     {
@@ -12,7 +13,7 @@ public class Panel_Button : MonoBehaviour
     }
     public void DoShowPanel() 
     {
-        _panel_Manager.ShowPanel(panelID);
+        _panel_Manager.ShowPanel(panelID, _Behaviour);
     }
 
     public void DoHidePanel() 
--- Assets/Scripts/Panel_Manger/Panel_Manager.cs
+++ Assets/Scripts/Panel_Manger/Panel_Manager.cs
@@ -18,14 +18,24 @@ public class Panel_Manager : Singleton<Panel_Manager>
         _object_Pool = Object_Pool.Instance;
     }
 
-    public void ShowPanel(string current_panelID) 
+    public void ShowPanel(string current_panelID, Panel_Behaviour behaviour = Panel_Behaviour.KEEP_PREVIOUS)
     {
         GameObject panel_Instance = _object_Pool.GetObjectFromPool(current_panelID);
 
-        if (panel_Instance != null) 
+        if (panel_Instance != null)
         {
+            if (behaviour == Panel_Behaviour.HIDE_PREVIOUS && GetAmountPanelsInQueue() > 0)
+            {
+                // Get the last element added to the list
+                var lastPanel = GetLastPanel();
+                if (lastPanel != null)
+                {
+                    lastPanel.panelInstace.SetActive(false);
+                }
+            }
+
             // Add this new Panel to the list
-            _ListInstances.Add(new Panel_Instance_Model {panelID = current_panelID,panelInstace = panel_Instance});
+            _ListInstances.Add(new Panel_Instance_Model { panelID = current_panelID, panelInstace = panel_Instance });
         }
         else
         {
@@ -33,28 +43,41 @@ public class Panel_Manager : Singleton<Panel_Manager>
         }
     }
 
-    public void HideLastPanel() 
+    public void HideLastPanel()
     {
         if (AnyPanelShowing())
         {
             // Get the last element added to the list
-            var lastPanel = _ListInstances[_ListInstances.Count - 1];
+            var lastPanel = GetLastPanel();
             _ListInstances.Remove(lastPanel);
 
             _object_Pool.PoolObject(lastPanel.panelInstace);
+
+            if (GetAmountPanelsInQueue() > 0)
+            {
+                lastPanel = GetLastPanel();
+                if (lastPanel != null && lastPanel.panelInstace.activeInHierarchy)
+                {
+                    lastPanel.panelInstace.SetActive(true);
+                }
+            }
         }
     }
 
+    private Panel_Instance_Model GetLastPanel()
+    {
+        return _ListInstances[_ListInstances.Count - 1];
+    }
 
     // Returns true if any panel is showing
-    public bool AnyPanelShowing() 
+    public bool AnyPanelShowing()
     {
         return GetAmountPanelsInQueue() > 0;
     }
 
 
     // Returns how many panels we have in the list
-    public int GetAmountPanelsInQueue() 
+    public int GetAmountPanelsInQueue()
     {
         return _ListInstances.Count;
     }
