cce460d8498a6fa6bba59467e862b389d838069e
Nicodragon25 <nicolasariellaure@gmail.com>
2023-04-30 15:58:10 -0300

    GameWindow Instances

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -6,8 +6,20 @@ public class PanelManager : MonoBehaviour
 {
     public void WindowInstantiate(GameObject windowPrefab)
     {
-
+        if (!windowPrefab.CompareTag("GameWindow"))
+        {
         GameObject newWindow =  Instantiate(windowPrefab, GameObject.Find("Canvas").transform);
+
+        }
+        else
+        {
+            GameObject gameWindow = GameObject.FindGameObjectWithTag("GameWindow");
+            if (gameWindow == null)
+            {
+                gameWindow = Instantiate(windowPrefab, GameObject.Find("Canvas").transform);
+            }
+        }
+
         //Instantiate()
         //newWindow.transform.SetParent(GameObject.Find("Canvas").transform);
     }
